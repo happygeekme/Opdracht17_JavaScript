abef1361f4dce205b21053e2a9f4abb3325cc809
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-19 10:59:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello Winc Academy");
+let name="Jitske";
+console.log(name)
+let y = 4;
+let z = 5;
+console.log(z=y%2)
+var age = 49;
+console.log(age)
+var leeftijd = "negenenveertig"
+console.log(leeftijd)
